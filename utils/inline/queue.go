619c22ec526bff39f8e06b8f5622d7a265ba8ecd
istkharroohi78 <istkharroohi@gmail.com>
2026-09-01 14:14:28 +0530

    Update queue.go

--- utils/inline/queue.go
+++ utils/inline/queue.go
@@ -11,8 +11,8 @@ import (
 func QueueMarkupTimer(langData map[string]string, duration string, cplay string, videoid string, played string, dur string) *tb.ReplyMarkup {
 	menu := &tb.ReplyMarkup{}
 
-	btnQueued := createBtn(menu, langData["QU_B_1"], fmt.Sprintf("GetQueued %s|%s", cplay, videoid), "")
-	btnClose := createBtn(menu, langData["CLOSE_BUTTON"], "close", "")
+	btnQueued := createBtn(menu, langData["QU_B_1"], fmt.Sprintf("GetQueued %s|%s", cplay, videoid), "", 0, "", false)
+	btnClose := createBtn(menu, langData["CLOSE_BUTTON"], "close", "", 0, "", false)
 
 	if duration == "Unknown" {
 		menu.Inline(menu.Row(btnQueued, btnClose))
@@ -27,7 +27,7 @@ func QueueMarkupTimer(langData map[string]string, duration string, cplay string,
 			timerText = fmt.Sprintf(timerText, played, dur)
 		}
 		
-		btnTimer := createBtn(menu, timerText, "GetTimer", "")
+		btnTimer := createBtn(menu, timerText, "GetTimer", "", 0, "", false)
 		menu.Inline(
 			menu.Row(btnTimer),
 			menu.Row(btnQueued, btnClose),
@@ -40,8 +40,8 @@ func QueueMarkupTimer(langData map[string]string, duration string, cplay string,
 func QueueBackMarkup(langData map[string]string, cplay string) *tb.ReplyMarkup {
 	menu := &tb.ReplyMarkup{}
 
-	btnBack := createBtn(menu, langData["BACK_BUTTON"], fmt.Sprintf("queue_back_timer %s", cplay), "")
-	btnClose := createBtn(menu, langData["CLOSE_BUTTON"], "close", "")
+	btnBack := createBtn(menu, langData["BACK_BUTTON"], fmt.Sprintf("queue_back_timer %s", cplay), "", 0, "", false)
+	btnClose := createBtn(menu, langData["CLOSE_BUTTON"], "close", "", 0, "", false)
 
 	menu.Inline(
 		menu.Row(btnBack, btnClose),
@@ -56,14 +56,14 @@ func AQMarkup(langData map[string]string, chatID int64) *tb.ReplyMarkup {
 
 	menu.Inline(
 		menu.Row(
-			createBtn(menu, "▷", "ADMIN Resume|"+cid, ""),
-			createBtn(menu, "II", "ADMIN Pause|"+cid, ""),
-			createBtn(menu, "↻", "ADMIN Replay|"+cid, ""),
-			createBtn(menu, "‣‣I", "ADMIN Skip|"+cid, ""),
-			createBtn(menu, "▢", "ADMIN Stop|"+cid, ""),
+			createBtn(menu, "▷", "ADMIN Resume|"+cid, "", 0, "", false),
+			createBtn(menu, "II", "ADMIN Pause|"+cid, "", 0, "", false),
+			createBtn(menu, "↻", "ADMIN Replay|"+cid, "", 0, "", false),
+			createBtn(menu, "‣‣I", "ADMIN Skip|"+cid, "", 0, "", false),
+			createBtn(menu, "▢", "ADMIN Stop|"+cid, "", 0, "", false),
 		),
 		menu.Row(
-			createBtn(menu, langData["CLOSE_BUTTON"], "close", ""),
+			createBtn(menu, langData["CLOSE_BUTTON"], "close", "", 0, "", false),
 		),
 	)
 	return menu
@@ -76,17 +76,17 @@ func QueueAdvancedMarkup(langData map[string]string, vidid string, chatID int64,
 	cid := fmt.Sprintf("%d", chatID)
 
 	menu.Inline(
-		menu.Row(createBtn(menu, langData["S_B_5"], "", fmt.Sprintf("https://t.me/%s?startgroup=true", botUsername))),
+		menu.Row(createBtn(menu, langData["S_B_5"], "", fmt.Sprintf("https://t.me/%s?startgroup=true", botUsername), 0, "", false)),
 		menu.Row(
-			createBtn(menu, "ᴘᴀᴜsᴇ", "ADMIN Pause|"+cid, ""),
-			createBtn(menu, "sᴛᴏᴘ", "ADMIN Stop|"+cid, ""),
-			createBtn(menu, "sᴋɪᴘ", "ADMIN Skip|"+cid, ""),
+			createBtn(menu, "ᴘᴀᴜsᴇ", "ADMIN Pause|"+cid, "", 0, "", false),
+			createBtn(menu, "sᴛᴏᴘ", "ADMIN Stop|"+cid, "", 0, "", false),
+			createBtn(menu, "sᴋɪᴘ", "ADMIN Skip|"+cid, "", 0, "", false),
 		),
 		menu.Row(
-			createBtn(menu, "ʀᴇsᴜᴍᴇ", "ADMIN Resume|"+cid, ""),
-			createBtn(menu, "ʀᴇᴘʟᴀʏ", "ADMIN Replay|"+cid, ""),
+			createBtn(menu, "ʀᴇsᴜᴍᴇ", "ADMIN Resume|"+cid, "", 0, "", false),
+			createBtn(menu, "ʀᴇᴘʟᴀʏ", "ADMIN Replay|"+cid, "", 0, "", false),
 		),
-		menu.Row(createBtn(menu, "ᴍᴏʀᴇ", "", "https://t.me/betabot_hub")),
+		menu.Row(createBtn(menu, "ᴍᴏʀᴇ", "", "https://t.me/betabot_hub", 0, "", false)),
 	)
 	return menu
 }
