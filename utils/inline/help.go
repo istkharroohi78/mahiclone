5743a8c7be11f2657b0a3a81d4b4ae853fb486c3
istkharroohi78 <istkharroohi@gmail.com>
2026-09-01 13:58:12 +0530

    Update help.go

--- utils/inline/help.go
+++ utils/inline/help.go
@@ -6,9 +6,6 @@ import (
 	"time"
 
 	tb "gopkg.in/tucnak/telebot.v2"
-
-	// Replace 'ANJALI' with your actual module path if different
-	"ANJALI/config"
 )
 
 // getRandomEmoji returns a random custom emoji ID
@@ -17,50 +14,37 @@ func getRandomEmoji() string {
 	return PremiumEmojis[rand.Intn(len(PremiumEmojis))]
 }
 
-// Smart Button Creator
-// In Telebot, we don't have ButtonStyle directly, but we can set text and payload
-func createBtn(menu *tb.ReplyMarkup, text string, cb string, url string) tb.Btn {
-	// Telebot doesn't natively support setting custom emoji IDs within the button struct easily
-	// but you can append the actual emoji character to the text if you have it.
-	// For now, we will construct the button.
-	if url != "" {
-		return menu.URL(text, url)
-	}
-	// "unique" prefix is required by telebot for callbacks
-	return menu.Data(text, cb)
-}
-
 func HelpPanel(langData map[string]string) *tb.ReplyMarkup {
 	menu := &tb.ReplyMarkup{}
 
 	first := []tb.Row{
 		menu.Row(
-			createBtn(menu, langData["H_B_1"], "help_callback_hb1", ""),
-			createBtn(menu, langData["H_B_2"], "help_callback_hb2", ""),
-			createBtn(menu, langData["H_B_3"], "help_callback_hb3", ""),
+			createBtn(menu, langData["H_B_1"], "help_callback_hb1", "", Primary, "", true),
+			createBtn(menu, langData["H_B_2"], "help_callback_hb2", "", Primary, "", true),
+			createBtn(menu, langData["H_B_3"], "help_callback_hb3", "", Primary, "", true),
 		),
 		menu.Row(
-			createBtn(menu, langData["H_B_4"], "help_callback_hb4", ""),
-			createBtn(menu, langData["H_B_5"], "help_callback_hb5", ""),
-			createBtn(menu, langData["H_B_6"], "help_callback_hb6", ""),
+			createBtn(menu, langData["H_B_4"], "help_callback_hb4", "", Primary, "", true),
+			createBtn(menu, langData["H_B_5"], "help_callback_hb5", "", Primary, "", true),
+			createBtn(menu, langData["H_B_6"], "help_callback_hb6", "", Primary, "", true),
 		),
 		menu.Row(
-			createBtn(menu, langData["H_B_7"], "help_callback_hb7", ""),
-			createBtn(menu, langData["H_B_8"], "help_callback_hb8", ""),
-			createBtn(menu, langData["H_B_9"], "help_callback_hb9", ""),
+			createBtn(menu, langData["H_B_7"], "help_callback_hb7", "", Primary, "", true),
+			createBtn(menu, langData["H_B_8"], "help_callback_hb8", "", Primary, "", true),
+			createBtn(menu, langData["H_B_9"], "help_callback_hb9", "", Primary, "", true),
 		),
 		menu.Row(
-			createBtn(menu, langData["H_B_10"], "help_callback_hb10", ""),
-			createBtn(menu, langData["H_B_11"], "help_callback_hb11", ""),
-			createBtn(menu, langData["H_B_12"], "help_callback_hb12", ""),
+			createBtn(menu, langData["H_B_10"], "help_callback_hb10", "", Primary, "", true),
+			createBtn(menu, langData["H_B_11"], "help_callback_hb11", "", Primary, "", true),
+			createBtn(menu, langData["H_B_12"], "help_callback_hb12", "", Primary, "", true),
 		),
 		menu.Row(
-			createBtn(menu, langData["H_B_13"], "help_callback_hb13", ""),
-			createBtn(menu, langData["H_B_14"], "help_callback_hb14", ""),
-			createBtn(menu, langData["H_B_15"], "help_callback_hb15", ""),
+			createBtn(menu, langData["H_B_13"], "help_callback_hb13", "", Primary, "", true),
+			createBtn(menu, langData["H_B_14"], "help_callback_hb14", "", Primary, "", true),
+			createBtn(menu, langData["H_B_15"], "help_callback_hb15", "", Primary, "", true),
 		),
 		menu.Row(
-			createBtn(menu, langData["BACK_BUTTON"], "settingsback_helper", ""),
+			createBtn(menu, langData["BACK_BUTTON"], "settingsback_helper", "", Danger, "", true),
 		),
 	}
 
@@ -73,32 +57,32 @@ func FirstPage(langData map[string]string, isOwner bool) *tb.ReplyMarkup {
 
 	first := []tb.Row{
 		menu.Row(
-			createBtn(menu, langData["H_B_1"], "help_callback_hb1", ""),
-			createBtn(menu, langData["H_B_2"], "help_callback_hb2", ""),
-			createBtn(menu, langData["H_B_3"], "help_callback_hb3", ""),
+			createBtn(menu, langData["H_B_1"], "help_callback_hb1", "", Primary, "", true),
+			createBtn(menu, langData["H_B_2"], "help_callback_hb2", "", Primary, "", true),
+			createBtn(menu, langData["H_B_3"], "help_callback_hb3", "", Primary, "", true),
 		),
 		menu.Row(
-			createBtn(menu, langData["H_B_11"], "help_callback_hb11", ""),
-			createBtn(menu, langData["H_B_8"], "help_callback_hb8", ""),
-			createBtn(menu, langData["H_B_6"], "help_callback_hb6", ""),
+			createBtn(menu, langData["H_B_11"], "help_callback_hb11", "", Primary, "", true),
+			createBtn(menu, langData["H_B_8"], "help_callback_hb8", "", Primary, "", true),
+			createBtn(menu, langData["H_B_6"], "help_callback_hb6", "", Primary, "", true),
 		),
 		menu.Row(
-			createBtn(menu, langData["H_B_13"], "help_callback_hb13", ""),
-			createBtn(menu, langData["H_B_12"], "help_callback_hb12", ""),
-			createBtn(menu, langData["H_B_9"], "help_callback_cloghelp", ""),
+			createBtn(menu, langData["H_B_13"], "help_callback_hb13", "", Primary, "", true),
+			createBtn(menu, langData["H_B_12"], "help_callback_hb12", "", Primary, "", true),
+			createBtn(menu, langData["H_B_9"], "help_callback_cloghelp", "", Primary, "", true),
 		),
 		menu.Row(
-			createBtn(menu, langData["H_B_10"], "help_callback_hb10", ""),
-			createBtn(menu, langData["H_B_14"], "help_callback_hb14", ""),
-			createBtn(menu, langData["H_B_15"], "help_callback_hb15", ""),
+			createBtn(menu, langData["H_B_10"], "help_callback_hb10", "", Primary, "", true),
+			createBtn(menu, langData["H_B_14"], "help_callback_hb14", "", Primary, "", true),
+			createBtn(menu, langData["H_B_15"], "help_callback_hb15", "", Primary, "", true),
 		),
 	}
 
 	if isOwner {
-		first = append(first, menu.Row(createBtn(menu, "🛠 ᴄʟᴏɴᴇ ғᴇᴀᴛᴜʀᴇ", "help_callback_chelp", "")))
+		first = append(first, menu.Row(createBtn(menu, "🛠 ᴄʟᴏɴᴇ ғᴇᴀᴛᴜʀᴇ", "help_callback_chelp", "", Success, "", true)))
 	}
 
-	first = append(first, menu.Row(createBtn(menu, langData["BACK_BUTTON"], "settingsback_home", "")))
+	first = append(first, menu.Row(createBtn(menu, langData["BACK_BUTTON"], "settingsback_home", "", Danger, "", true)))
 	
 	menu.Inline(first...)
 	return menu
@@ -108,17 +92,17 @@ func CloneHelpPanel(langData map[string]string) *tb.ReplyMarkup {
 	menu := &tb.ReplyMarkup{}
 
 	buttons := []tb.Row{
-		menu.Row(createBtn(menu, "ᴍᴀɴᴀɢᴇ", "help_callback_clone_manage", "")),
+		menu.Row(createBtn(menu, "ᴍᴀɴᴀɢᴇ", "help_callback_clone_manage", "", Primary, "", true)),
 		menu.Row(
-			createBtn(menu, "sᴛᴀʀᴛ", "help_callback_clone_start", ""),
-			createBtn(menu, "ᴘɪɴɢ", "help_callback_clone_ping", ""),
+			createBtn(menu, "sᴛᴀʀᴛ", "help_callback_clone_start", "", Primary, "", true),
+			createBtn(menu, "ᴘɪɴɢ", "help_callback_clone_ping", "", Primary, "", true),
 		),
 		menu.Row(
-			createBtn(menu, "ᴘʟᴀʏ ᴍᴏᴅᴇ", "help_callback_clone_play", ""),
-			createBtn(menu, "ʟᴏɢɢᴇʀ", "help_callback_clone_logger", ""),
+			createBtn(menu, "ᴘʟᴀʏ ᴍᴏᴅᴇ", "help_callback_clone_play", "", Primary, "", true),
+			createBtn(menu, "ʟᴏɢɢᴇʀ", "help_callback_clone_logger", "", Primary, "", true),
 		),
-		menu.Row(createBtn(menu, "ʙᴜᴛᴛᴏɴs & ʀᴇɴᴀᴍᴇ", "help_callback_clone_buttons", "")),
-		menu.Row(createBtn(menu, langData["BACK_BUTTON"], "settings_back_helper", "")),
+		menu.Row(createBtn(menu, "ʙᴜᴛᴛᴏɴs & ʀᴇɴᴀᴍᴇ", "help_callback_clone_buttons", "", Primary, "", true)),
+		menu.Row(createBtn(menu, langData["BACK_BUTTON"], "settings_back_helper", "", Danger, "", true)),
 	}
 	
 	menu.Inline(buttons...)
@@ -127,18 +111,18 @@ func CloneHelpPanel(langData map[string]string) *tb.ReplyMarkup {
 
 func CloneBackMarkup(langData map[string]string) *tb.ReplyMarkup {
 	menu := &tb.ReplyMarkup{}
-	menu.Inline(menu.Row(createBtn(menu, langData["BACK_BUTTON"], "help_callback_chelp", "")))
+	menu.Inline(menu.Row(createBtn(menu, langData["BACK_BUTTON"], "help_callback_chelp", "", Danger, "", true)))
 	return menu
 }
 
 func HelpBackMarkup(langData map[string]string) *tb.ReplyMarkup {
 	menu := &tb.ReplyMarkup{}
-	menu.Inline(menu.Row(createBtn(menu, langData["BACK_BUTTON"], "settings_back_helper", "")))
+	menu.Inline(menu.Row(createBtn(menu, langData["BACK_BUTTON"], "settings_back_helper", "", Danger, "", true)))
 	return menu
 }
 
 func PrivateHelpPanel(langData map[string]string, botUsername string) *tb.ReplyMarkup {
 	menu := &tb.ReplyMarkup{}
-	menu.Inline(menu.Row(createBtn(menu, langData["S_B_4"], "", fmt.Sprintf("https://t.me/%s?start=help", botUsername))))
+	menu.Inline(menu.Row(createBtn(menu, langData["S_B_4"], "", fmt.Sprintf("https://t.me/%s?start=help", botUsername), Primary, "", true)))
 	return menu
 }
