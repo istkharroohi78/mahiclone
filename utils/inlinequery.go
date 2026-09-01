628a02e2dfdb6b9bf63d4d2908a3364b66f20b0b
istkharroohi78 <istkharroohi@gmail.com>
2026-09-01 13:13:24 +0530

    Update inlinequery.go

--- utils/inlinequery.go
+++ utils/inlinequery.go
@@ -8,58 +8,46 @@ import (
 func GetStaticInlineAnswers() tb.Results {
 	return tb.Results{
 		&tb.ArticleResult{
-			ResultBase: tb.ResultBase{
-				ID:          "pause",
-				Title:       "Pᴀᴜsᴇ",
-				Description: "ᴩᴀᴜsᴇ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-				ThumbURL:    "https://telegra.ph/file/c5952790fa8235f499749.jpg",
-			},
-			Text: "/pause",
+			ResultBase:  tb.ResultBase{ID: "pause"},
+			Title:       "Pᴀᴜsᴇ",
+			Description: "ᴩᴀᴜsᴇ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+			ThumbURL:    "https://telegra.ph/file/c5952790fa8235f499749.jpg",
+			Text:        "/pause",
 		},
 		&tb.ArticleResult{
-			ResultBase: tb.ResultBase{
-				ID:          "resume",
-				Title:       "Rᴇsᴜᴍᴇ",
-				Description: "ʀᴇsᴜᴍᴇ ᴛʜᴇ ᴩᴀᴜsᴇᴅ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-				ThumbURL:    "https://telegra.ph/file/c5952790fa8235f499749.jpg",
-			},
-			Text: "/resume",
+			ResultBase:  tb.ResultBase{ID: "resume"},
+			Title:       "Rᴇsᴜᴍᴇ",
+			Description: "ʀᴇsᴜᴍᴇ ᴛʜᴇ ᴩᴀᴜsᴇᴅ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+			ThumbURL:    "https://telegra.ph/file/c5952790fa8235f499749.jpg",
+			Text:        "/resume",
 		},
 		&tb.ArticleResult{
-			ResultBase: tb.ResultBase{
-				ID:          "skip",
-				Title:       "Sᴋɪᴩ",
-				Description: "sᴋɪᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-				ThumbURL:    "https://telegra.ph/file/c5952790fa8235f499749.jpg",
-			},
-			Text: "/skip",
+			ResultBase:  tb.ResultBase{ID: "skip"},
+			Title:       "Sᴋɪᴩ",
+			Description: "sᴋɪᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+			ThumbURL:    "https://telegra.ph/file/c5952790fa8235f499749.jpg",
+			Text:        "/skip",
 		},
 		&tb.ArticleResult{
-			ResultBase: tb.ResultBase{
-				ID:          "end",
-				Title:       "Eɴᴅ",
-				Description: "ᴇɴᴅ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-				ThumbURL:    "https://telegra.ph/file/c5952790fa8235f499749.jpg",
-			},
-			Text: "/end",
+			ResultBase:  tb.ResultBase{ID: "end"},
+			Title:       "Eɴᴅ",
+			Description: "ᴇɴᴅ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+			ThumbURL:    "https://telegra.ph/file/c5952790fa8235f499749.jpg",
+			Text:        "/end",
 		},
 		&tb.ArticleResult{
-			ResultBase: tb.ResultBase{
-				ID:          "shuffle",
-				Title:       "Sʜᴜғғʟᴇ",
-				Description: "sʜᴜғғʟᴇ ᴛʜᴇ ǫᴜᴇᴜᴇᴅ sᴏɴɢs ɪɴ ᴩʟᴀʏʟɪsᴛ.",
-				ThumbURL:    "https://telegra.ph/file/c5952790fa8235f499749.jpg",
-			},
-			Text: "/shuffle",
+			ResultBase:  tb.ResultBase{ID: "shuffle"},
+			Title:       "Sʜᴜғғʟᴇ",
+			Description: "sʜᴜғғʟᴇ ᴛʜᴇ ǫᴜᴇᴜᴇᴅ sᴏɴɢs ɪɴ ᴩʟᴀʏʟɪsᴛ.",
+			ThumbURL:    "https://telegra.ph/file/c5952790fa8235f499749.jpg",
+			Text:        "/shuffle",
 		},
 		&tb.ArticleResult{
-			ResultBase: tb.ResultBase{
-				ID:          "loop",
-				Title:       "Lᴏᴏᴩ",
-				Description: "ʟᴏᴏᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ ᴛʀᴀᴄᴋ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-				ThumbURL:    "https://telegra.ph/file/c5952790fa8235f499749.jpg",
-			},
-			Text: "/loop 3",
+			ResultBase:  tb.ResultBase{ID: "loop"},
+			Title:       "Lᴏᴏᴩ",
+			Description: "ʟᴏᴏᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ ᴛʀᴀᴄᴋ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+			ThumbURL:    "https://telegra.ph/file/c5952790fa8235f499749.jpg",
+			Text:        "/loop 3",
 		},
 	}
 }
