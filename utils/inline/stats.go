ec0ee4ca3180c267b4421bcaaacd3c45ab347f41
istkharroohi78 <istkharroohi@gmail.com>
2026-09-02 07:45:47 +0530

    Update stats.go

--- utils/inline/stats.go
+++ utils/inline/stats.go
@@ -10,18 +10,18 @@ func StatsButtons(loc map[string]string, status bool) *tb.ReplyMarkup {
 	var topRow []tb.Btn
 	if status {
 		topRow = []tb.Btn{
-			CreateBtn(m, loc["SA_B_2"], "bot_stats_sudo", "", Primary, 0, false),
-			CreateBtn(m, loc["SA_B_3"], "TopOverall", "", Primary, 0, false),
+			createBtn(m, loc["SA_B_2"], "bot_stats_sudo", "", Primary, "", false),
+			createBtn(m, loc["SA_B_3"], "TopOverall", "", Primary, "", false),
 		}
 	} else {
 		topRow = []tb.Btn{
-			CreateBtn(m, loc["SA_B_1"], "TopOverall", "", Primary, 0, false),
+			createBtn(m, loc["SA_B_1"], "TopOverall", "", Primary, "", false),
 		}
 	}
 
 	m.Inline(
 		m.Row(topRow...),
-		m.Row(CreateBtn(m, loc["CLOSE_BUTTON"], "close", "", Danger, 0, false)),
+		m.Row(createBtn(m, loc["CLOSE_BUTTON"], "close", "", Danger, "", false)),
 	)
 	return m
 }
@@ -30,8 +30,8 @@ func BackStatsButtons(loc map[string]string) *tb.ReplyMarkup {
 	m := &tb.ReplyMarkup{}
 	m.Inline(
 		m.Row(
-			CreateBtn(m, loc["BACK_BUTTON"], "stats_back", "", Primary, 0, false),
-			CreateBtn(m, loc["CLOSE_BUTTON"], "close", "", Danger, 0, false),
+			createBtn(m, loc["BACK_BUTTON"], "stats_back", "", Primary, "", false),
+			createBtn(m, loc["CLOSE_BUTTON"], "close", "", Danger, "", false),
 		),
 	)
 	return m
