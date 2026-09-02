42b50b10f79ee2ea7a2329c787d7c947bdeb3a14
istkharroohi78 <istkharroohi@gmail.com>
2026-09-02 14:07:05 +0530

    Update restart.go

--- plugins/sudo/restart.go
+++ plugins/sudo/restart.go
@@ -3,6 +3,7 @@ package sudo
 import (
 	"os"
 
+	"ANJALI/utils"
 	"ANJALI/utils/inline"
 
 	tb "gopkg.in/tucnak/telebot.v2"
@@ -10,25 +11,25 @@ import (
 
 func RegisterRestartHandlers(b *tb.Bot) {
 	b.Handle("/restart", func(m *tb.Message) {
-		if !IsSudoer(int64(m.Sender.ID)) {
+		if !utils.IsSudoer(int64(m.Sender.ID)) {
 			return
 		}
 
 		markup := &tb.ReplyMarkup{}
 		markup.Inline(
 			markup.Row(
-				inline.CreateBtn(markup, "🤖 ᴍᴀɪɴ ʙᴏᴛ", "restart_main", "", inline.Primary, 0, false),
-				inline.CreateBtn(markup, "👥 ᴄʟᴏɴᴇs ʙᴏᴛ", "restart_clones", "", inline.Primary, 0, false),
+				inline.CreateBtn(markup, "🤖 ᴍᴀɪɴ ʙᴏᴛ", "restart_main", "", 0, "", false),
+				inline.CreateBtn(markup, "👥 ᴄʟᴏɴᴇs ʙᴏᴛ", "restart_clones", "", 0, "", false),
 			),
-			markup.Row(inline.CreateBtn(markup, "🔄 ʙᴏᴛʜ (ᴍᴀɪɴ + ᴄʟᴏɴᴇs)", "restart_both", "", inline.Primary, 0, false)),
-			markup.Row(inline.CreateBtn(markup, "❌ ᴄᴀɴᴄᴇʟ", "cancel_restart", "", inline.Danger, 0, false)),
+			markup.Row(inline.CreateBtn(markup, "🔄 ʙᴏᴛʜ (ᴍᴀɪɴ + ᴄʟᴏɴᴇs)", "restart_both", "", 0, "", false)),
+			markup.Row(inline.CreateBtn(markup, "❌ ᴄᴀɴᴄᴇʟ", "cancel_restart", "", 0, "", false)),
 		)
 
 		b.Send(m.Chat, "**⚠️ ᴋɪsᴇ ʀᴇsᴛᴀʀᴛ ᴋᴀʀɴᴀ ᴄʜᴀʜᴛᴇ ʜᴀɪɴ?**\n\n*(Neeche diye gaye options me se select karein)*", markup, tb.ModeMarkdown)
 	})
 
 	b.Handle("\frestart_main", func(c *tb.Callback) {
-		if !IsSudoer(int64(c.Sender.ID)) {
+		if !utils.IsSudoer(int64(c.Sender.ID)) {
 			return
 		}
 		b.Edit(c.Message, "🔄 **ʀᴇsᴛᴀʀᴛɪɴɢ ᴍᴀɪɴ ʙᴏᴛ...**")
@@ -36,7 +37,7 @@ func RegisterRestartHandlers(b *tb.Bot) {
 	})
 
 	b.Handle("\fcancel_restart", func(c *tb.Callback) {
-		if !IsSudoer(int64(c.Sender.ID)) {
+		if !utils.IsSudoer(int64(c.Sender.ID)) {
 			return
 		}
 		b.Edit(c.Message, "**❌ ʀᴇsᴛᴀʀᴛ ᴘʀᴏᴄᴇss ᴄᴀɴᴄᴇʟʟᴇᴅ.**")
